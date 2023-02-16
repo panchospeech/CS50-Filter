9234b7de7668b59f25f294801a61347b05d43600
Francisco Blümel <panchospeech@gmail.com>
2023-02-16 12:30:12 -0300

    Add files via upload

---
+++ helpers.c
@@ -0,0 +1,154 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    //Go through each row
+    for (int i = 0; i < height; i++)
+    {
+        //Go through each column
+        for (int j = 0; j < width; j++)
+        {
+            //Convert pixels to float
+            float Red = image[i][j].rgbtRed;
+            float Green = image[i][j].rgbtGreen;
+            float Blue = image[i][j].rgbtBlue;
+
+            //Find the average value and assign it to RGB
+            int average = round((Red + Green + Blue) / 3.0);
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Go through each row
+    for (int i = 0; i < height; i++)
+    {
+        // Go through each column
+        for (int j = 0; j < width; j++)
+        {
+            // Convert pixels to float
+            float originalRed = image[i][j].rgbtRed;
+            float originalGreen = image[i][j].rgbtGreen;
+            float originalBlue = image[i][j].rgbtBlue;
+
+            // Find the updated pixel value
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+
+            // Update the pixel value if sepiaRed, sepiaGreen or sepiaBlue > 255
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            //Update the final pixel values
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+
+        }
+    }
+
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    //Go through each row
+    for (int i = 0; i < height; i++)
+    {
+        //Go through each column
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap between the first pixel [i][j] with the last one [i][width - (j + 1)]
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = temp;
+        }
+
+    }
+
+
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    //Create a temp copy of the image (where to store new pixel values as we loop)
+    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //Copy each pixel
+            temp[i][j] = image[i][j];
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int totalRed, totalBlue, totalGreen;
+            totalRed = totalBlue = totalGreen = 0;
+            float counter = 0.00;
+
+            //Get neighbouring pixels (relative position from 0,0)
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    int currentX = i + x;
+                    int currentY = j + y;
+
+                    //Check if neighbouring pixel is valid (if its not on the edge)
+                    if (currentX < 0 || currentX > (height - 1) || currentY < 0 || currentY > (width - 1))
+                    {
+                        continue;
+                    }
+
+                    // Get image value
+                    totalRed += image[currentX][currentY].rgbtRed;
+                    totalGreen += image[currentX][currentY].rgbtGreen;
+                    totalBlue += image[currentX][currentY].rgbtBlue;
+
+                    counter++;
+                }
+
+                //Calculate average if neighbouring pixels
+                temp[i][j].rgbtRed = round(totalRed / counter);
+                temp[i][j].rgbtGreen = round(totalGreen / counter);
+                temp[i][j].rgbtBlue = round(totalBlue / counter);
+            }
+        }
+    }
+
+    // Copy new pixels into original image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //Copy each pixel
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+            image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
+        }
+    }
+    return;
+}
